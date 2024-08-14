e360344f8c8f3ac100c5f281a48b3a3a0894806a
yuuuuuuuz <fff.yuzu.3156@gmail.com>
2024-08-14 22:34:27 +0900

    PancakeMaker修正

--- Assets/Script/Pancake/PancakeMaker.cs
+++ Assets/Script/Pancake/PancakeMaker.cs
@@ -14,27 +14,18 @@ public class PancakeMaker : MonoBehaviour
     public void PancakeMake(){
         // パンケーキオブジェクトを生成、画面に表示
         GameObject pancake = Instantiate(pancakePrefab, transform.position, Quaternion.identity);
-        pancake.GetComponent<Pancake>().SetPancakeMaker(this); // パンケーキにPancakeMakerをセット
+        
+        // パンケーキにPancakeMakerをセット
+        Pancake pancakeScript = pancake.GetComponent<PancakeMake>();
+        if (pancakeScript != null){
+            pancakeScript.SetPancakeMaker(this);
+
+            // 生成したパンケーキオブジェクトにGameMasterをセット
+            pancakeScript.SetGameMaster(_gameMaster);
+        }
     }
 
     public void SetGameMaster(GameMaster master){
         _gameMaster = master;
-    }
-
-    // 焼き目確認
-    public void Baked(PancakePart part){
-        if(part.IsBaked()){
-            // 全パーツに焼き目が付いているか確認し次の処理へ
-            // 必要に応じて全体の焼け状態を管理するコードを追加
-            Debug.Log("baked");
-        }
-    }
-
-    // 焦げ確認
-    public void Burnt(PancakePart part){
-        if(part.IsBurnt()){
-            Debug.Log("burnt");
-            _gameMaster.ReduceLife(); // ゲームマスターにライフを減らす指示を出す
-        }
     }
 }
