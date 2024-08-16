01715c66a0f46f9b5f40ca22179b805def5ae009
NagataKentaro <86968899+NagataKentaro@users.noreply.github.com>
2024-08-16 23:03:43 +0900

    ロコミコメントを生成するスクリプトの作成
    星1～5に応じてコメントを生成します。
    presenterは動いていないです。
    reviewCommentMakerはデバッグ用なのでまともに適当です。

---
+++ Assets/Script/UI/Viewer/ReviewCommentViewer.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using TMPro;
+using UnityEngine.UI;
+using DG.Tweening;
+using UnityEngine.UIElements.Experimental;
+
+public class ReviewCommentViewer : MonoBehaviour
+{
+    private TextAsset csvFile;
+    
+    private List<string[]> csvData = new List<string[]>();
+
+    //canvasGroupはpanelがもってあるのでパネルをアタッチしてください
+    [SerializeField] private CanvasGroup commentCanvasGroup;
+    [SerializeField] private TextMeshProUGUI starText;
+    [SerializeField] private TextMeshProUGUI commentText;
+
+    private RectTransform canvasRectTransform;
+    private void Start()
+    {
+        csvFile = Resources.Load("reviewComment") as TextAsset;
+        StringReader reader = new StringReader(csvFile.text);
+
+        while (reader.Peek() != -1)
+        {
+            string line = reader.ReadLine();
+            csvData.Add(line.Split(','));
+        }
+
+        commentCanvasGroup.DOFade(0.0f, 0.0f);
+        canvasRectTransform = commentCanvasGroup.gameObject.GetComponent<RectTransform>();
+        canvasRectTransform.DOAnchorPos(new Vector2(0.0f,-10.0f), 1.0f);
+    }
+    
+    //コメントに対応したテキストを書き込む
+    public void ShowComment(float review)
+    {
+        int reviewStar = Mathf.RoundToInt(review);
+        if (reviewStar > 5)
+        {
+            reviewStar = 5;
+        }
+        else if (reviewStar < 1)
+        {
+            reviewStar = 1;
+        }
+        string stringReviewStar = reviewStar.ToString();
+        
+        
+        //StringReviewStar(星の数)に対応したコメントを全て検索する。
+        var reviewCommnets = csvData.FindAll(x => x[0] == stringReviewStar);
+        
+        int randomNum = Random.Range(0, reviewCommnets.Count);
+        starText.text = $"星{review:f1}";
+        commentText.text = reviewCommnets[randomNum][1];
+        CommentMove();
+    }
+    
+   //ポップアウトにコメントを動かす関数
+   void CommentMove()
+   {
+       Sequence sequence = DOTween.Sequence();
+       sequence
+           .Append(commentCanvasGroup.DOFade(1.0f, 1.0f))
+           .Join(canvasRectTransform.DOAnchorPos(new Vector2(0.0f, 0.0f), 1.0f))
+           .AppendInterval(1.0f)
+           .Append(commentCanvasGroup.DOFade(0.0f, 1.0f))
+           .Join(canvasRectTransform.DOAnchorPos(new Vector2(0.0f, -10.0f), 1.0f));
+   }
+}
