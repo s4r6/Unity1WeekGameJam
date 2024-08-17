a0467ac51b6f630a4ca2a820a0085b732f4c36b5
yuuuuuuuz <fff.yuzu.3156@gmail.com>
2024-08-17 22:25:17 +0900

    火の大きさがGameMasterのfirePowerに連動する

--- Assets/Script/Fire/Fire.cs
+++ Assets/Script/Fire/Fire.cs
@@ -8,11 +8,13 @@ public class Fire : MonoBehaviour
     public float speed = 5f; // 揺れの速さ
     public float sizeChangeSpeed = 2f;
     public float fireSize = 1f;
+    private GameMaster _gameMaster;
     private float _randomOffset;
     private Vector3 _initialScale;
     private Vector3 _targetScale;
 
     void Start(){
+        _gameMaster = FindObjectOfType<GameMaster>();
         _randomOffset = Random.Range(0f, 2f * Mathf.PI);
         _initialScale = transform.localScale;
         _targetScale = _initialScale;
@@ -21,6 +23,12 @@ public class Fire : MonoBehaviour
     void Update(){
         float angle = rotationAmount * Mathf.Sin(Time.time * speed + _randomOffset);
         transform.rotation = Quaternion.Euler(0f, 0f, angle);
+
+        if (_gameMaster != null)
+        {
+            float firePower = _gameMaster.GetFire();
+            Debug.Log("Current FirePower: " + firePower);
+        }
 
         // デバッグ用
         if (Input.GetKey(KeyCode.A)){
@@ -36,8 +44,8 @@ public class Fire : MonoBehaviour
         }
     }
 
-    public void ChangeFire(float newFireSize){
-        _targetScale = _initialScale * newFireSize;
+    public void ChangeFire(float firePower){
+        _targetScale = _initialScale * firePower;
         transform.localScale = Vector3.Lerp(transform.localScale, _targetScale, Time.deltaTime * sizeChangeSpeed);
     }
 }
