85a7498104714de7742be9614ac6de2655384fb0
NagataKentaro <86968899+NagataKentaro@users.noreply.github.com>
2024-08-18 03:16:13 +0900

    Revert "Merge pull request #62 from InnovaGameCreate/fix/MainGameUI"
    This reverts commit c96e1c10ae9c351edc5851d5b942868fa7669f11, reversing
    changes made to f8ebacee968dc308f566ec7eda567a461f26043e.

--- Assets/Script/UI/Presenter/ReviewCommentPresenter.cs
+++ Assets/Script/UI/Presenter/ReviewCommentPresenter.cs
@@ -1,15 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UniRx;
 
 public class ReviewCommentPresenter : MonoBehaviour
 {
-    [SerializeField] private GameMaster gameMaster;
     [SerializeField] private ReviewCommentViewer reviewCommentViewer;
     // Start is called before the first frame update
     void Start()
     {
-        gameMaster.OnComment.Subscribe(x => reviewCommentViewer.ShowComment(x));
+        //reviewCommentViewer.ShowComment(3);
     }
 }
